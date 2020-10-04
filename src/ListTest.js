84546f4d477fa94fefa7a48793658801597f98de
Michelle <65927092+mismyl123@users.noreply.github.com>
2020-10-04 16:52:03 -0400

    Update ListTest.js

--- src/ListTest.js
+++ src/ListTest.js
@@ -1,11 +1,31 @@
+
 import React from 'react';
-import ReactDOM from "react-dom";
-import List from './List';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List.js';
+import STORE from './STORE.js'
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<List cards = {[]} />, div);
+it('renders without crashing', () =>{
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <List 
+      key={STORE.lists[0].id}
+      header={STORE.lists[0].header}
+      cards={STORE.lists[0].cardIds.map(id => STORE.allCards[id])}
+    />,
+    div
+  )
 
-    //clean up code
-    ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.unmountComponentAtNode(div);
 })
+
+it('renders as expected', () => {
+  const tree = renderer
+    .create( <List 
+      key={STORE.lists[0].id}
+      header={STORE.lists[0].header}
+      cards={STORE.lists[0].cardIds.map(id => STORE.allCards[id])}
+    />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
