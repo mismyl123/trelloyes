679e1bf0d20a9fe48ba886e997148769e471b31f
Michelle <65927092+mismyl123@users.noreply.github.com>
2020-10-04 22:16:46 -0400

    Update List.js

--- src/List.js
+++ src/List.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Card from './Card'
 import './List.css';
-
 export default function List(props) {
   return (
     <section className='List'>
@@ -12,17 +11,24 @@ export default function List(props) {
         {props.cards.map((card) =>
           <Card
             key={card.id}
+            id={card.id}
             title={card.title}
             content={card.content}
+            onClickDelete={props.onClickDelete}
           />
         )}
         <button
           type='button'
           className='List-add-button'
+          onClick={() => props.onClickAdd(props.id)}
         >
           + Add Random Card
         </button>
       </div>
     </section>
   )
+}
+
+List.defaultProps = {
+  onClickAdd: () => {},
 }
